c23e95cb577d76670f989074ea8c6b9e9afbefe6
Matthieu Rouet <mrouet75@gmail.com>
2025-07-29 10:41:48 +0100

    init commit

---
+++ examples/precompute/fd_traj_ocp.py
@@ -0,0 +1,315 @@
+import pinocchio as pin
+import example_robot_data as erd
+import aligator
+import numpy as np
+from aligator import manifolds, constraints, dynamics
+
+import time
+
+from pinocchio.visualize import MeshcatVisualizer as meshviz
+
+from precompute_utils import create_contact_phases, plot_torques
+
+robot = erd.load("go2")
+
+
+robot = erd.load("go2")
+rmodel: pin.Model = robot.model
+rdata: pin.Data = rmodel.createData()
+
+nq = rmodel.nq
+nv = rmodel.nv
+nu = nv - 6     # nb d'actionneurs - freeFlyerJoint(6 DoF) = nb actionneurs commandables
+
+q1 = rmodel.referenceConfigurations["standing"]
+
+q2 = q1.copy()
+q2[2] = 0.5
+
+# update positions
+pin.forwardKinematics(rmodel, rdata, q1)
+pin.updateFramePlacements(rmodel, rdata)
+
+# TODO: add collision with the ground !!
+FOOT_FRAME_IDS = {
+    fname: rmodel.getFrameId(fname) for fname in ["FL_foot", "FR_foot", "RL_foot", "RR_foot"]
+}
+FOOT_JOINT_IDS = {
+    fname: rmodel.frames[fid].parentJoint for fname, fid in FOOT_FRAME_IDS.items()
+}
+
+print(FOOT_JOINT_IDS)
+
+umax = rmodel.effortLimit[6:]
+for i in range(len(umax)):
+    umax[i] -= 5
+
+umin = - umax
+
+
+constraint_models = []
+constraint_datas = []
+for fname, fid in FOOT_FRAME_IDS.items():
+    joint_id = FOOT_JOINT_IDS[fname]
+    pl1 = rmodel.frames[fid].placement
+    pl2 = rdata.oMf[fid]
+    cm = pin.RigidConstraintModel(
+        pin.ContactType.CONTACT_3D,
+        rmodel,
+        joint_id,
+        pl1,
+        0,
+        pl2,
+        pin.LOCAL_WORLD_ALIGNED,
+    )
+    cm.corrector.Kp[:] = (0, 0, 100)
+    cm.corrector.Kd[:] = (50, 50, 50)
+    constraint_models.append(cm)
+    constraint_datas.append(cm.createData())
+
+
+FL_id = rmodel.getFrameId("FL_foot")
+FR_id = rmodel.getFrameId("FR_foot")
+RL_id = rmodel.getFrameId("RL_foot")
+RR_id = rmodel.getFrameId("RR_foot")
+
+
+# -------------- Simulation -------------- #
+
+dt = 0.01 # timestep = 10 ms
+
+# settings to solve contacts
+proxSettings = pin.ProximalSettings(1e-9, 1e-10, 10)
+
+space = manifolds.MultibodyPhaseSpace(rmodel)
+
+space_standing = np.concatenate((q1, np.zeros(nv)))
+space_jumping = np.concatenate((q2, np.zeros(nv)))
+
+com0 = pin.centerOfMass(rmodel, rdata, q1)
+
+# weights
+w_x = np.array([
+    1000,   # base pos
+    1000,
+    1000,
+
+    1,   # body orientation
+    1,
+    1,
+
+    100,   # FL leg
+    100,
+    100,
+
+    100,   # FR leg
+    100,
+    100,
+
+    100,   #RL leg
+    100,
+    100,
+
+    100,   #RR leg
+    100,
+    100,
+
+    10,   # body vel
+    10,
+    10,
+    10,
+    10,
+    10,
+
+    10,   # FL vel
+    10,
+    10,
+
+    10,   # FR vel
+    10,
+    10,
+
+    10,   # RL vel
+    10,
+    10,
+
+    10,   #RR vel
+    10,
+    10
+])
+
+w_x = np.diag(w_x)
+w_u = np.eye(nu)*1e-1   # pas trop surtout
+
+w_com = 100 * np.ones(3)
+w_com = np.diag(w_com)
+
+# actuation matrix
+S = np.eye(nv, nu, -6)  # cast u cmd to q
+
+# create velocity constraints
+v_ref = pin.Motion().Zero()
+
+
+# frame velocities to create constraints on landing
+frame_vel_FL = aligator.FrameVelocityResidual(
+    space.ndx, nu, rmodel, v_ref, FL_id, pin.LOCAL_WORLD_ALIGNED
+)[:2]
+frame_vel_FR = aligator.FrameVelocityResidual(
+    space.ndx, nu, rmodel, v_ref, FR_id, pin.LOCAL_WORLD_ALIGNED
+)[:2]
+
+frame_vel_RL = aligator.FrameVelocityResidual(
+    space.ndx, nu, rmodel, v_ref, RL_id, pin.LOCAL_WORLD_ALIGNED
+)[:2]
+frame_vel_RR = aligator.FrameVelocityResidual(
+    space.ndx, nu, rmodel, v_ref, RR_id, pin.LOCAL_WORLD_ALIGNED
+)[:2]
+
+FL_placement = rdata.oMf[FL_id]
+FR_placement = rdata.oMf[FR_id]
+RL_placement = rdata.oMf[RL_id]
+RR_placement = rdata.oMf[RR_id]
+
+frame_fn_FL = aligator.FramePlacementResidual(
+    space.ndx, nu, rmodel, FL_placement, FL_id
+)[2]
+
+frame_fn_FR = aligator.FramePlacementResidual(
+    space.ndx, nu, rmodel, FR_placement, FR_id
+)[2]
+
+frame_fn_RL = aligator.FramePlacementResidual(
+    space.ndx, nu, rmodel, RL_placement, RL_id
+)[2]
+
+frame_fn_RR = aligator.FramePlacementResidual(
+    space.ndx, nu, rmodel, RR_placement, RR_id
+)[2]
+
+
+#############################
+
+T_ds = 50
+T_ss = 50
+
+#############################
+
+
+possible_contacts = {"stand":[True, True, True, True],
+                     "FL_up":[False, False, True, True],    # Front legs up
+                     "air":[False, False, False, False],    # jumping
+                     "RL_up":[True, True, False, False],    # Back legs up
+                    }
+
+
+c_phases = ["stand", "air", "stand"]
+
+
+timings = [T_ds, T_ss, T_ds]
+cycles = 1  # number of repetitions of the sequence
+
+# get the contacts
+contact_phases = [possible_contacts[c] for c in create_contact_phases(c_phases, timings, cycles)]
+
+T_mpc = len(contact_phases)    # nombre de résolutions d'ocp  ~ temps de la simu
+
+
+def create_dynamics(contact_type):
+    """ Creates full dynamics model for each stage of the OCP """
+    if contact_type == possible_contacts["stand"]:
+        ode = dynamics.MultibodyConstraintFwdDynamics(space, S, constraint_models, proxSettings)
+
+    elif contact_type == possible_contacts["air"]:
+        ode = dynamics.MultibodyConstraintFwdDynamics(space, S, [], proxSettings)
+
+    else:
+        # return error, this should not happen
+        raise ValueError ("Invalid contact type for dynamics creation.")
+
+    dyn_model = dynamics.IntegratorEuler(ode, dt)
+    return dyn_model
+
+
+def createStage(contact, i):
+    """ Creates each stage of the OCP, i.e. running cost & constraints for each simulation time step"""
+
+    # create all costs
+    state_cost = aligator.StateErrorResidual(space, nu, space_standing)
+    ctrl_cost = aligator.ControlErrorResidual(space.ndx, nu)
+
+    # create a running cost instance
+    rcost = aligator.CostStack(space, nu)   
+
+    # add all costs to the running cost
+    rcost.addCost("state_cost", aligator.QuadraticStateCost(state_cost, w_x))
+    rcost.addCost("control_cost", aligator.QuadraticControlCost(space, nu, w_u))
+
+    stm = aligator.StageModel(rcost, create_dynamics(contact))
+
+    # add box constraints for forces
+    # stm.addConstraint()
+    ctrl_fn = aligator.ControlErrorResidual(space.ndx, np.zeros(nu))
+    stm.addConstraint(ctrl_fn, constraints.BoxConstraint(umin, umax))
+
+    # on landing, feet velocities = 0
+    if i == T_ds + T_ss:
+        stm.addConstraint(frame_vel_FL, constraints.EqualityConstraintSet())
+        stm.addConstraint(frame_vel_FR, constraints.EqualityConstraintSet())
+        stm.addConstraint(frame_vel_RL, constraints.EqualityConstraintSet())
+        stm.addConstraint(frame_vel_RR, constraints.EqualityConstraintSet())
+
+        stm.addConstraint(frame_fn_FL, constraints.EqualityConstraintSet())
+        stm.addConstraint(frame_fn_FR, constraints.EqualityConstraintSet())
+        stm.addConstraint(frame_fn_RL, constraints.EqualityConstraintSet())
+        stm.addConstraint(frame_fn_RR, constraints.EqualityConstraintSet())
+
+    return stm
+
+
+stages = []
+for i in range(T_mpc):
+    stages.append(createStage(contact_phases[i], i))
+
+term_cost = aligator.CostStack(space, nu)
+
+problem = aligator.TrajOptProblem(space_standing, stages, term_cost)
+
+# terminal constraints
+com_pos = aligator.CentroidalCoMResidual(space.ndx, nu, com0)
+com_pos = aligator.LinearFunctionComposition(com_pos, np.diag(np.array([0,0,1]))) # just z component
+term_stage_cstr = aligator.StageConstraint(com_pos, constraints.EqualityConstraintSet())
+problem.addTerminalConstraint(term_stage_cstr)
+
+
+TOL = 1e-2
+mu_init = 1e-8
+max_iters = 500
+verbose = aligator.VerboseLevel.VERBOSE
+solver = aligator.SolverProxDDP(TOL, mu_init,max_iters, verbose=verbose)
+
+# init solver !
+solver.setup(problem)
+
+# values of u and x throughout simulation
+us_init = [np.zeros(nu) for _ in range(T_mpc)]
+xs_init = [space_standing for _ in range(T_mpc + 1)]
+
+# run sim
+conv = solver.run(problem, xs_init, us_init)
+
+res = solver.results
+print(res)
+
+plot_torques(np.array(res.us))
+
+def play_results():
+    viz = meshviz(rmodel, robot.collision_model, robot.visual_model)
+    viz.initViewer(open=True)
+    viz.loadViewerModel()
+    viz.display(q1)
+    time.sleep(4)
+    qs = [x[:nq] for x in res.xs]
+    viz.play(qs, dt)
+
+play_results()
---
+++ examples/precompute/preOcpMpc.py
@@ -0,0 +1,405 @@
+import numpy as np
+from bullet_robot import BulletRobot
+from simple_mpc import (
+    RobotModelHandler,
+    RobotDataHandler,
+    FullDynamicsOCP,
+    MPC,
+    IDSolver,
+    Interpolator,
+    FrictionCompensation
+)
+import example_robot_data as erd
+import pinocchio as pin
+import time
+from utils import extract_forces
+import copy
+
+import matplotlib.pyplot as plt
+
+# ####### CONFIGURATION  ############
+# Load robot
+URDF_SUBPATH = "/go2_description/urdf/go2.urdf"
+base_joint_name ="root_joint"
+robot_wrapper = erd.load('go2')
+
+# Create Model and Data handler
+model_handler = RobotModelHandler(robot_wrapper.model, "standing", base_joint_name)
+model_handler.addFoot("FL_foot", base_joint_name, pin.XYZQUATToSE3(np.array([ 0.17, 0.15, 0.0, 0,0,0,1])))
+model_handler.addFoot("FR_foot", base_joint_name, pin.XYZQUATToSE3(np.array([ 0.17,-0.15, 0.0, 0,0,0,1])))
+model_handler.addFoot("RL_foot", base_joint_name, pin.XYZQUATToSE3(np.array([-0.24, 0.15, 0.0, 0,0,0,1])))
+model_handler.addFoot("RR_foot", base_joint_name, pin.XYZQUATToSE3(np.array([-0.24,-0.15, 0.0, 0,0,0,1])))
+data_handler = RobotDataHandler(model_handler)
+
+nq = model_handler.getModel().nq
+nv = model_handler.getModel().nv
+nu = nv - 6
+force_size = 3
+nk = len(model_handler.getFeetNames())
+nf = force_size
+
+gravity = np.array([0, 0, -9.81])
+fref = np.zeros(force_size)
+fref[2] = -model_handler.getMass() / nk * gravity[2]
+u0 = np.zeros(model_handler.getModel().nv - 6)
+
+w_basepos = [0, 0, 0, 0, 0, 0]
+w_legpos = [10, 10, 10]
+
+w_basevel = [10, 10, 10, 10, 10, 10]
+w_legvel = [0.1, 0.1, 0.1]
+w_x = np.array(w_basepos + w_legpos * 4 + w_basevel + w_legvel * 4)
+w_cent_lin = np.array([0.0, 0.0, 0])
+w_cent_ang = np.array([0, 0, 0])
+w_forces_lin = np.array([0.0001, 0.0001, 0.0001])
+w_frame = np.eye(3)*1e3
+
+dt = 0.01
+problem_conf = dict(
+    timestep=dt,
+    w_x=np.diag(w_x),
+    w_u=np.eye(u0.size) * 1e-4,
+    w_cent=np.diag(np.concatenate((w_cent_lin, w_cent_ang))),
+    gravity=gravity,
+    force_size=3,
+    w_forces=np.diag(w_forces_lin),
+    w_frame=w_frame,
+    umin=-model_handler.getModel().effortLimit[6:],
+    umax=model_handler.getModel().effortLimit[6:],
+    qmin=model_handler.getModel().lowerPositionLimit[7:],
+    qmax=model_handler.getModel().upperPositionLimit[7:],
+    Kp_correction=np.array([0, 0, 20]),
+    Kd_correction=np.array([100, 100, 100]),
+    mu=0.8,
+    Lfoot=0.01,
+    Wfoot=0.01,
+    torque_limits=True,
+    kinematics_limits=True,
+    force_cone=False,
+    land_cstr=True
+)
+T = 50
+
+dynproblem = FullDynamicsOCP(problem_conf, model_handler)
+dynproblem.createProblem(model_handler.getReferenceState(), T, force_size, gravity[2], False)
+
+##########################
+T_ds = 50
+T_ss = 30
+##########################
+
+T_lift = 10
+T_land = 2
+
+N_simu = int(0.01 / 0.001)
+mpc_conf = dict(
+    support_force=-model_handler.getMass() * gravity[2],
+    TOL=1e-4,
+    mu_init=1e-8,
+    max_iters=1,
+    num_threads=1,
+    swing_apex=0.2,
+    T_fly=T_ss,
+    T_contact=T_ds,
+    timestep=dt,
+)
+
+mpc = MPC(mpc_conf, dynproblem)
+
+""" Define contact sequence throughout horizon"""
+contact_phase_quadru = {
+    "FL_foot": True,
+    "FR_foot": True,
+    "RL_foot": True,
+    "RR_foot": True,
+}
+contact_phase_lift_FL = {
+    "FL_foot": False,
+    "FR_foot": True,
+    "RL_foot": True,
+    "RR_foot": False,
+}
+contact_phase_lift_FR = {
+    "FL_foot": True,
+    "FR_foot": False,
+    "RL_foot": False,
+    "RR_foot": True,
+}
+contact_phase_lift = {
+    "FL_foot": False,
+    "FR_foot": False,
+    "RL_foot": False,
+    "RR_foot": False,
+}
+
+contact_phase_lift_Front = {
+    "FL_foot": False,
+    "FR_foot": False,
+    "RL_foot": True,
+    "RR_foot": True,
+}
+# contact_phases = [contact_phase_quadru] * T_ds
+# contact_phases += [contact_phase_lift_FL] * T_ss
+# contact_phases += [contact_phase_quadru] * T_ds
+# contact_phases += [contact_phase_lift_FR] * T_ss
+
+contact_phases = [contact_phase_quadru] * int(T_ds / 2)
+contact_phases += [contact_phase_lift] * T_ss
+contact_phases += [contact_phase_quadru] * int(T_ds / 2)
+
+mpc.generateCycleHorizon(contact_phases)
+
+""" Initialize whole-body inverse dynamics QP"""
+contact_ids = model_handler.getFeetIds()
+id_conf = dict(
+    contact_ids=contact_ids,
+    x0=model_handler.getReferenceState(),
+    mu=0.8,
+    Lfoot=0.01,
+    Wfoot=0.01,
+    force_size=3,
+    kd=0,
+    w_force=0,
+    w_acc=0,
+    w_tau=1,
+    verbose=False,
+)
+
+qp = IDSolver(id_conf, model_handler.getModel())
+
+""" Friction """
+fcompensation = FrictionCompensation(model_handler.getModel(), True)
+""" Interpolation """
+interpolator = Interpolator(model_handler.getModel())
+
+""" Initialize simulation"""
+device = BulletRobot(
+    model_handler.getModel().names,
+    erd.getModelPath(URDF_SUBPATH),
+    URDF_SUBPATH,
+    1e-3,
+    model_handler.getModel(),
+    model_handler.getReferenceState()[:3],
+)
+
+device.initializeJoints(model_handler.getReferenceState()[:nq])
+
+for i in range(40):
+    device.setFrictionCoefficients(i, 10, 0)
+#device.changeCamera(1.0, 60, -15, [0.6, -0.2, 0.5])
+
+q_meas, v_meas = device.measureState()
+x_measured  = np.concatenate([q_meas, v_meas])
+mpc.getDataHandler().updateInternalData(x_measured, False)
+
+ref_foot_pose = [mpc.getDataHandler().getRefFootPose(mpc.getModelHandler().getFeetNames()[i]) for i in range(4)]
+for pose in ref_foot_pose:
+    pose.translation[2] = 0
+device.showQuadrupedFeet(*ref_foot_pose)
+Tmpc = len(contact_phases)
+
+force_FL = []
+force_FR = []
+force_RL = []
+force_RR = []
+FL_measured = []
+FR_measured = []
+RL_measured = []
+RR_measured = []
+FL_references = []
+FR_references = []
+RL_references = []
+RR_references = []
+x_multibody = []
+u_multibody = []
+com_measured = []
+solve_time = []
+L_measured = []
+
+torques = []
+torques_before_qp = []
+
+# vitesse du robot
+v = np.zeros(6)
+v[0] = 0
+mpc.velocity_base = v
+
+# number of simulation steps
+n_steps = 200
+
+comp_times = [50]
+
+for t in range(n_steps):
+    print("Time " + str(t))
+    land_LF = mpc.getFootLandCycle("FL_foot")
+    land_RF = mpc.getFootLandCycle("RL_foot")
+    takeoff_LF = mpc.getFootTakeoffCycle("FL_foot")
+    takeoff_RF = mpc.getFootTakeoffCycle("RL_foot")
+
+
+    device.moveQuadrupedFeet(
+        mpc.getReferencePose(0, "FL_foot").translation,
+        mpc.getReferencePose(0, "FR_foot").translation,
+        mpc.getReferencePose(0, "RL_foot").translation,
+        mpc.getReferencePose(0, "RR_foot").translation,
+    )
+
+    start = time.time()
+    mpc.iterate(x_measured)
+    end = time.time()
+    solve_time.append(end - start)
+
+    # comparison
+    if t in comp_times:
+        res = np.array(mpc.xs)
+        
+
+    a0 = mpc.getStateDerivative(0)[nv:]
+    a1 = mpc.getStateDerivative(1)[nv:]
+
+    forces_vec0 = mpc.getContactForces(0)
+    forces_vec1 = mpc.getContactForces(1)
+    contact_states = mpc.ocp_handler.getContactState(0)
+
+    force_FL.append(forces_vec0[:3])
+    force_FR.append(forces_vec0[3:6])
+    force_RL.append(forces_vec0[6:9])
+    force_RR.append(forces_vec0[9:12])
+
+    forces = [forces_vec0, forces_vec1]
+    ddqs = [a0, a1]
+    xss = [mpc.xs[0], mpc.xs[1]]
+    uss = [mpc.us[0], mpc.us[1]]
+
+    FL_measured.append(mpc.getDataHandler().getFootPose("FL_foot").translation)
+    FR_measured.append(mpc.getDataHandler().getFootPose("FR_foot").translation)
+    RL_measured.append(mpc.getDataHandler().getFootPose("RL_foot").translation)
+    RR_measured.append(mpc.getDataHandler().getFootPose("RR_foot").translation)
+    FL_references.append(mpc.getReferencePose(0, "FL_foot").translation)
+    FR_references.append(mpc.getReferencePose(0, "FR_foot").translation)
+    RL_references.append(mpc.getReferencePose(0, "RL_foot").translation)
+    RR_references.append(mpc.getReferencePose(0, "RR_foot").translation)
+    com_measured.append(mpc.getDataHandler().getData().com[0].copy())
+    L_measured.append(mpc.getDataHandler().getData().hg.angular.copy())
+
+
+    for j in range(N_simu):
+        # time.sleep(0.01)
+        delay = j / float(N_simu) * dt
+
+        x_interp = interpolator.interpolateState(delay, dt, xss)
+        u_interp = interpolator.interpolateLinear(delay, dt, uss)
+        acc_interp = interpolator.interpolateLinear(delay, dt, ddqs)
+        force_interp = interpolator.interpolateLinear(delay, dt, forces)
+
+        q_meas, v_meas = device.measureState()
+        x_measured = np.concatenate([q_meas, v_meas])
+
+        mpc.getDataHandler().updateInternalData(x_measured, True)
+
+        current_torque = u_interp - 1. * mpc.Ks[0] @ model_handler.difference(
+            x_measured, x_interp
+        )
+
+        qp.solveQP(
+            mpc.getDataHandler().getData(),
+            contact_states,
+            x_measured[nq:],
+            acc_interp,
+            current_torque,
+            force_interp,
+            mpc.getDataHandler().getData().M,
+        )
+
+        torques_before_qp.append(current_torque)
+
+        # actually useless
+        qp_torque = qp.solved_torque.copy()
+
+        torques.append(qp_torque)
+
+        # not needed, just need some right Ricatti gains
+        friction_torque = fcompensation.computeFriction(x_interp[nq + 6:], qp_torque)
+        device.execute(friction_torque)
+
+        u_multibody.append(copy.deepcopy(current_torque))
+        x_multibody.append(x_measured)
+
+force_FL = np.array(force_FL)
+force_FR = np.array(force_FR)
+force_RL = np.array(force_RL)
+force_RR = np.array(force_RR)
+solve_time = np.array(solve_time)
+FL_measured = np.array(FL_measured)
+FR_measured = np.array(FR_measured)
+RL_measured = np.array(RL_measured)
+RR_measured = np.array(RR_measured)
+FL_references = np.array(FL_references)
+FR_references = np.array(FR_references)
+RL_references = np.array(RL_references)
+RR_references = np.array(RR_references)
+com_measured = np.array(com_measured)
+L_measured = np.array(L_measured)
+
+torques = np.array(torques)
+
+def plot_forces_with_bounds( force_limit=100):
+    time = np.arange(force_FL.shape[0])
+    fig, axs = plt.subplots(4, 1, figsize=(12, 10), sharex=True)
+    feet = ['FL', 'FR', 'RL', 'RR']
+    forces = [force_FL, force_FR, force_RL, force_RR]
+    for i, (ax, f, name) in enumerate(zip(axs, forces, feet)):
+        ax.plot(time, f[:, 0], label=f'{name} Fx')
+        ax.plot(time, f[:, 1], label=f'{name} Fy')
+        ax.plot(time, f[:, 2], label=f'{name} Fz')
+        ax.axhline(force_limit, color='r', linestyle='--', label='Force limit')
+        ax.axhline(-force_limit, color='r', linestyle='--')
+        ax.set_ylabel(f'{name} Force [N]')
+        ax.legend()
+        ax.grid(True)
+    axs[-1].set_xlabel('Time step')
+    plt.tight_layout()
+    plt.show()
+
+# plot_forces_with_bounds()
+
+n_joints = 12
+n_legs = 4
+
+torques = np.array(torques)
+torques_limits = np.array(model_handler.getModel().effortLimit[6:])
+
+with open("examples/qptorques.npy", "wb") as f:
+    np.save(f, torques)
+    np.save(f, torques_limits)
+
+time = np.arange(torques.shape[0])
+tau1 = np.array([t[0] for t in torques])
+tau2 = np.array([t[0] for t in torques_before_qp])
+
+
+plt.plot(time,tau2-tau1, label="abs(diff)")
+plt.grid(True)
+plt.legend()
+plt.title("différence des torques avant et après QP")
+plt.savefig("examples/diff_tau.png")
+plt.show()
+
+fig, axs = plt.subplots(2,1,figsize=(12,10), sharex=True)
+axs[0].plot(time, tau2, label="tau before QP")
+axs[1].plot(time, tau1, label="tau after QP")
+limite = torques_limits[0]
+axs[0].axhline(limite, linestyle="--")
+axs[0].axhline(-limite, linestyle="--")
+axs[1].axhline(limite, linestyle="--")
+axs[1].axhline(-limite, linestyle="--")
+axs[-1].set_xlabel('Time step')
+plt.tight_layout()
+plt.savefig("examples/tau1_tau2.png")
+plt.show()
+
+# plot_torques_with_bounds()
+
+""" save_trajectory(x_multibody, u_multibody, com_measured, force_FL, force_FR, force_RL, force_RR, solve_time,
+                FL_measured, FR_measured, RL_measured, RR_measured,
+                FL_references, FR_references, RL_references, RR_references, L_measured, "fulldynamics") """
